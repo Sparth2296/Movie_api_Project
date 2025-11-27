11cf9c685eb6c4354e8337711a2989c4b6768d3a
sparth2296 <solankiparth94274@gmail.com>
2025-11-27 20:43:30 +0530

    Movie Projectgit add .

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const router = require("../router/movie");
 // Views & static
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
-app.use(express.static(path.join(__dirname, "../assets")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
